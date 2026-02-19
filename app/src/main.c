b4e3e315009a773f69b0438177526a249daa8461
Richard Wheatley <richard.wheatley@ambiq.com>
2026-02-19 14:08:20 -0600

    app: update main for the ambiq-common-application
    add ambiq-based sample
    remove the nucleo board
    update proj.conf for ambiq-common-application
    
    Signed-off-by: Richard Wheatley <richard.wheatley@ambiq.com>

--- app/src/main.c
+++ app/src/main.c
@@ -1,80 +1,198 @@
-/*
- * Copyright (c) 2021 Nordic Semiconductor ASA
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
-#include <zephyr/logging/log.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/__assert.h>
+#include <string.h>
+#include <inttypes.h>
 
-#include <app/drivers/blink.h>
+/* size of stack area used by each thread */
+#define STACKSIZE 1024
 
-#include <app_version.h>
+/* scheduling priority used by each thread */
+#define PRIORITY 7
 
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+#define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
 
-#define BLINK_PERIOD_MS_STEP 100U
-#define BLINK_PERIOD_MS_MAX  1000U
+#if !DT_NODE_HAS_STATUS_OKAY(LED0_NODE)
+#error "Unsupported board: led0 devicetree alias is not defined"
+#endif
 
-int main(void)
+#if !DT_NODE_HAS_STATUS_OKAY(LED1_NODE)
+#error "Unsupported board: led1 devicetree alias is not defined"
+#endif
+
+#define SLEEP_TIME_MS	1
+
+/*
+ * Get button configuration from the devicetree sw0 alias. This is mandatory.
+ */
+#define SW0_NODE	DT_ALIAS(sw0)
+#if !DT_NODE_HAS_STATUS_OKAY(SW0_NODE)
+#error "Unsupported board: sw0 devicetree alias is not defined"
+#endif
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
+							      {0});
+static struct gpio_callback button_cb_data;
+
+/*
+ * The led0 devicetree alias is optional. If present, we'll use it
+ * to turn on the LED whenever the button is pressed.
+ */
+static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios,
+						     {0});
+
+struct printk_data_t {
+	void *fifo_reserved; /* 1st word reserved for use by fifo */
+	uint32_t gpio;
+	uint32_t count;
+};
+
+K_FIFO_DEFINE(printk_fifo);
+
+struct led {
+	struct gpio_dt_spec spec;
+	uint8_t num;
+};
+
+static const struct led led0 = {
+	.spec = GPIO_DT_SPEC_GET_OR(LED0_NODE, gpios, {0}),
+	.num = 0,
+};
+
+static const struct led led1 = {
+	.spec = GPIO_DT_SPEC_GET_OR(LED1_NODE, gpios, {0}),
+	.num = 1,
+};
+
+void blink(const struct led *led, uint32_t sleep_ms, uint32_t id)
 {
+	const struct gpio_dt_spec *spec = &led->spec;
+	int cnt = 0;
 	int ret;
-	unsigned int period_ms = BLINK_PERIOD_MS_MAX;
-	const struct device *sensor, *blink;
-	struct sensor_value last_val = { 0 }, val;
 
-	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
-
-	sensor = DEVICE_DT_GET(DT_NODELABEL(example_sensor));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
-		return 0;
+	if (!device_is_ready(spec->port)) {
+		printk("Error: %s device is not ready\n", spec->port->name);
+		return;
 	}
 
-	blink = DEVICE_DT_GET(DT_NODELABEL(blink_led));
-	if (!device_is_ready(blink)) {
-		LOG_ERR("Blink LED not ready");
-		return 0;
+	ret = gpio_pin_configure_dt(spec, GPIO_OUTPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure pin %d (LED '%d')\n",
+			ret, spec->pin, led->num);
+		return;
 	}
-
-	ret = blink_off(blink);
-	if (ret < 0) {
-		LOG_ERR("Could not turn off LED (%d)", ret);
-		return 0;
-	}
-
-	printk("Use the sensor to change LED blinking period\n");
 
 	while (1) {
-		ret = sensor_sample_fetch(sensor);
-		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
-			return 0;
-		}
+		gpio_pin_set(spec->port, spec->pin, cnt % 2);
 
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return 0;
-		}
+		struct printk_data_t tx_data = { .gpio = id, .count = cnt };
 
-		if ((last_val.val1 == 0) && (val.val1 == 1)) {
-			if (period_ms == 0U) {
-				period_ms = BLINK_PERIOD_MS_MAX;
-			} else {
-				period_ms -= BLINK_PERIOD_MS_STEP;
-			}
+		size_t size = sizeof(struct printk_data_t);
+		char *mem_ptr = k_malloc(size);
+		__ASSERT_NO_MSG(mem_ptr != 0);
 
-			printk("Proximity detected, setting LED period to %u ms\n",
-			       period_ms);
-			blink_set_period_ms(blink, period_ms);
-		}
+		memcpy(mem_ptr, &tx_data, size);
 
-		last_val = val;
+		k_fifo_put(&printk_fifo, mem_ptr);
 
-		k_sleep(K_MSEC(100));
+		k_msleep(sleep_ms);
+		cnt++;
 	}
-
-	return 0;
 }
 
+void blink0_thread(void)
+{
+	blink(&led0, 2000, 0);
+}
+
+void blink1_thread(void)
+{
+	blink(&led1, 5000, 1);
+}
+
+void uart_out_thread(void)
+{
+	while (1) {
+		struct printk_data_t *rx_data = k_fifo_get(&printk_fifo,
+							   K_FOREVER);
+		printk("Toggled led%d; counter=%d\n",
+		       rx_data->gpio, rx_data->count);
+		k_free(rx_data);
+	}
+}
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb,
+		    uint32_t pins)
+{
+	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+}
+
+void gpio_thread(void)
+{
+	int status;
+
+	if (!gpio_is_ready_dt(&button)) {
+		printk("Error: button device %s is not ready\n",
+		       button.port->name);
+	}
+
+	status = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (status != 0) {
+		printk("Error %d: failed to configure %s pin %d\n",
+		       status, button.port->name, button.pin);
+	}
+
+	status = gpio_pin_interrupt_configure_dt(&button,
+					      GPIO_INT_EDGE_TO_ACTIVE);
+	if (status != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n",
+			status, button.port->name, button.pin);
+	}
+
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
+	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
+
+	if (led.port && !gpio_is_ready_dt(&led)) {
+		printk("Error %d: LED device %s is not ready; ignoring it\n",
+		       status, led.port->name);
+		led.port = NULL;
+	}
+	if (led.port) {
+		status = gpio_pin_configure_dt(&led, GPIO_OUTPUT);
+		if (status != 0) {
+			printk("Error %d: failed to configure LED device %s pin %d\n",
+			       status, led.port->name, led.pin);
+			led.port = NULL;
+		} else {
+			printk("Set up LED at %s pin %d\n", led.port->name, led.pin);
+		}
+	}
+
+	printk("Press the button\n");
+	if (led.port) {
+		while (1) {
+			/* If we have an LED, match its state to the button's. */
+			int val = gpio_pin_get_dt(&button);
+
+			if (val >= 0) {
+				gpio_pin_set_dt(&led, val);
+			}
+			k_msleep(SLEEP_TIME_MS);
+	
+        }
+	}
+}
+
+
+K_THREAD_DEFINE(blink0_id, STACKSIZE, blink0_thread, NULL, NULL, NULL,
+		PRIORITY, 0, 0);
+K_THREAD_DEFINE(blink1_id, STACKSIZE, blink1_thread, NULL, NULL, NULL,
+		PRIORITY, 0, 0);
+K_THREAD_DEFINE(uart_out_id, STACKSIZE, uart_out_thread, NULL, NULL, NULL,
+		PRIORITY, 0, 0);
+K_THREAD_DEFINE(gpio_id, STACKSIZE, gpio_thread, NULL, NULL, NULL,
+        PRIORITY, 0, 0);
